71fef013973fdf7e823ebbac3aa89032e38d86ac
baka4n <qq474899581@gmail.com>
2024-10-24 15:33:55 +0800

    test new methods

--- src/main/java/org/polaris_bear/wild_wind/datagen/ModItemModelProvider.java
+++ src/main/java/org/polaris_bear/wild_wind/datagen/ModItemModelProvider.java
@@ -9,6 +9,9 @@ import net.neoforged.neoforge.registries.DeferredHolder;
 import org.polaris_bear.wild_wind.WildWindMod;
 import org.polaris_bear.wild_wind.common.init.ModItems;
 
+import java.util.Collection;
+import java.util.function.Consumer;
+
 public class ModItemModelProvider extends ItemModelProvider {
 
     public ModItemModelProvider(PackOutput output, ExistingFileHelper existingFileHelper) {
@@ -18,13 +21,24 @@ public class ModItemModelProvider extends ItemModelProvider {
     @SuppressWarnings("unchecked")
     @Override
     protected void registerModels() {
-        ModItems.ITEMS
-                .getEntries()
-                .stream()
-                .filter(holder -> holder.get() instanceof DeferredSpawnEggItem)
-                .map(holder -> (DeferredHolder<Item, DeferredSpawnEggItem>) holder).forEach(holder -> withExistingParent(holder.getId().getPath(), mcLoc("item/template_spawn_egg")));
-
+//        ModItems.ITEMS
+//                .getEntries()
+//                .stream()
+//                .filter(holder -> holder.get() instanceof DeferredSpawnEggItem)
+//                .map(holder -> (DeferredHolder<Item, DeferredSpawnEggItem>) holder).forEach(holder -> withExistingParent(holder.getId().getPath(), mcLoc("item/template_spawn_egg")));
+        isSame(ModItems.ITEMS.getEntries(), DeferredSpawnEggItem.class, holder -> {
+            withExistingParent(holder.getId().getPath(), mcLoc("item/template_spawn_egg"));
+        });
 
 //        withExistingParent(ModItems.FIREFLY_SPAWN_EGG.getId().getPath(), mcLoc("item/template_spawn_egg"));
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T extends Item> void isSame(Collection<DeferredHolder<Item, ? extends Item>> items, Class<DeferredSpawnEggItem> tClass, Consumer<DeferredHolder<Item, T>> consumer) {
+        items
+                .stream()
+                .filter(holder -> holder.get().getClass().equals(tClass))
+                .map(holder -> (DeferredHolder<Item, T>) holder)
+                .forEach(consumer);
     }
 }
