f5a42a48d67f258b20558e3b81dd9baf638c3cbc
baka4n <qq474899581@gmail.com>
2024-11-02 20:07:06 +0800

    推

--- processor/annotation/src/main/java/module-info.java
+++
@@ -1,5 +0,0 @@
-module wild.wind.annotation.main {
-    exports org.polaris2023.annotation;
-    exports org.polaris2023.annotation.config;
-
-}
--- processor/src/main/java/org/polaris2023/plugin/InitPlugin.java
+++ processor/src/main/java/org/polaris2023/plugin/InitPlugin.java
@@ -4,6 +4,10 @@ import com.sun.source.tree.ClassTree;
 import com.sun.source.tree.MethodTree;
 import com.sun.source.util.*;
 
+import com.sun.tools.javac.util.Context;
+import com.sun.tools.javac.api.BasicJavacTask;
+import org.polaris2023.plugin.listeners.InitListener;
+
 import java.util.Arrays;
 
 public class InitPlugin implements Plugin {
@@ -15,26 +19,7 @@ public class InitPlugin implements Plugin {
     @Override
     public void init(JavacTask task, String... args) {
         System.out.printf("Initializing plugin %s with args %s%n", getName(), Arrays.toString(args));
-        task.addTaskListener(new TaskListener() {
-            @Override
-            public void finished(TaskEvent e) {
-                if (e.getKind() == TaskEvent.Kind.ENTER) {
-                    e.getCompilationUnit().accept(new TreeScanner<Void, Void>() {
-                        @Override
-                        public Void visitClass(ClassTree node, Void unused) {
-
-                            System.out.println(node.getSimpleName());
-                            return super.visitClass(node, unused);
-                        }
-
-                        @Override
-                        public Void visitMethod(MethodTree node, Void unused) {
-
-                            return super.visitMethod(node, unused);
-                        }
-                    }, null);
-                }
-            }
-        });
+        Context ctx = ((BasicJavacTask) task).getContext();
+        task.addTaskListener(new InitListener(ctx));
     }
 }
