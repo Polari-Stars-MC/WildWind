500a2409215ba9eda5ed18635f1af86537b688ab
baka4n <qq474899581@gmail.com>
2024-11-02 14:54:05 +0800

    注解保留机制

--- processor/annotation/src/main/java/org/polaris2023/annotation/config/Push.java
+++ processor/annotation/src/main/java/org/polaris2023/annotation/config/Push.java
@@ -3,7 +3,7 @@ package org.polaris2023.annotation.config;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
-@Retention(RetentionPolicy.SOURCE)
+@Retention(RetentionPolicy.CLASS)
 public @interface Push {
     String value();
 }
