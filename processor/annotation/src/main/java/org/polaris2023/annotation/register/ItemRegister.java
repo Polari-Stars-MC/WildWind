0801d9bb6128db6e9ac3c9ac82144ec7cb24ed94
baka4n <qq474899581@gmail.com>
2024-11-07 15:58:41 +0800

    javac进度++

--- processor/annotation/src/main/java/org/polaris2023/annotation/register/ItemRegister.java
+++ processor/annotation/src/main/java/org/polaris2023/annotation/register/ItemRegister.java
@@ -8,19 +8,5 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.SOURCE)
 @Target(ElementType.FIELD)
 public @interface ItemRegister {
-    String value() default "ModItems";//generation name
-    String[] simpleItem() default {};// simple items
-    ItemEntry[] entry();
-    @interface ItemEntry {
-        String[] value();// register name
-        String className() default "net.minecraft.world.item.Item";// class name
-    }
-
-    @interface SpawnEgg {
-        String value();// register name
-    }
-
-    @interface Properties {
-
-    }
+    String name();// simple item
 }
--- processor/src/main/java/org/polaris2023/plugin/InitPlugin.java
+++ processor/src/main/java/org/polaris2023/plugin/InitPlugin.java
@@ -2,11 +2,24 @@ package org.polaris2023.plugin;
 
 import com.google.auto.service.AutoService;
 import com.sun.source.util.*;
+import com.sun.tools.javac.api.*;
+import com.sun.tools.javac.util.*;
+import com.sun.tools.javac.tree.*;
+import org.polaris2023.utils.Unsafe;
+
 import java.util.Arrays;
 
 
 @AutoService(Plugin.class)
 public class InitPlugin implements Plugin {
+    private Context context;
+    private Log log = null;
+    private Trees trees;
+    private TreeMaker treeMaker;
+    static {
+        Unsafe.exportJdkModule();
+    }
+
     @Override
     public String getName() {
         return "PolarisInit";
@@ -14,6 +27,11 @@ public class InitPlugin implements Plugin {
 
     @Override
     public void init(JavacTask task, String... args) {
-        System.out.printf("Initializing plugin %s with args %s%n", getName(), Arrays.toString(args));
+
+        context = ((BasicJavacTask) task).getContext();
+        log = Log.instance(context);
+        trees = Trees.instance(task);
+        treeMaker = TreeMaker.instance(context);
+        log.printRawLines("Initializing plugin %s args %s".formatted(getName(), Arrays.toString(args)));
     }
 }
