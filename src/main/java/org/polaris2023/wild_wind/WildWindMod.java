c8e74aec4465b3f1aad4fa1bb0d898aae30cca65
baka4n <qq474899581@gmail.com>
2024-11-22 19:40:07 +0800

    更多的食物属性

--- src/main/java/org/polaris2023/wild_wind/WildWindMod.java
+++ src/main/java/org/polaris2023/wild_wind/WildWindMod.java
@@ -6,6 +6,7 @@ import net.minecraft.core.component.DataComponentType;
 import net.minecraft.core.component.DataComponents;
 import net.minecraft.core.component.TypedDataComponent;
 import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.food.Foods;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
 import net.neoforged.bus.api.IEventBus;
@@ -21,6 +22,7 @@ import org.polaris2023.wild_wind.common.init.ModFoods;
 import org.polaris2023.wild_wind.util.interfaces.IConfig;
 
 import java.util.ServiceLoader;
+import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 @Mod(WildWindMod.MOD_ID)
@@ -40,7 +42,19 @@ public class WildWindMod {
             food(Items.RED_MUSHROOM, ModFoods.RED_MUSHROOM);
             food(Items.CRIMSON_FUNGUS, ModFoods.CRIMSON_FUNGUS);
             food(Items.WARPED_FUNGUS, ModFoods.WARPED_FUNGUS);
+            food(Items.BEETROOT_SEEDS, ModFoods.SEEDS);
+            food(Items.MELON_SEEDS, ModFoods.SEEDS);
+            food(Items.PUMPKIN_SEEDS, ModFoods.SEEDS);
+            food(Items.WHEAT_SEEDS, ModFoods.SEEDS);
+            food(Items.TORCHFLOWER_SEEDS, ModFoods.POISON_SEEDS);
+            food(Items.PITCHER_POD, ModFoods.POISON_SEEDS);
+            food(Items.SUGAR_CANE, ModFoods.SUGAR_CANE);
+            food(Items.SUGAR, ModFoods.SUGAR);
+            food(Items.FERMENTED_SPIDER_EYE, ModFoods.FERMENTED_SPIDER_EYE);
+            food(Items.GLISTERING_MELON_SLICE, ModFoods.GLISTERING_MELON_SLICE);
+            food(Items.MILK_BUCKET, ModFoods.MILK);
             component(Items.SLIME_BALL, ModComponents.SLIME_COLOR, 0);
+
         });
 
         for (var iConfig : ServiceLoader.load(IConfig.class))
@@ -48,16 +62,21 @@ public class WildWindMod {
     }
 
     private static void food(Item item, Supplier<FoodProperties> food) {
-        DataComponentMap.Builder builder = DataComponentMap.builder();
-        builder.addAll(item.components);
-        builder.set(DataComponents.FOOD, food.get());
-        item.components = builder.build();
+        component(item, builder -> {
+            builder.set(DataComponents.FOOD, food.get());
+        });
     }
 
     private static <T> void component(Item item, Supplier<DataComponentType<T>> type, T t) {
+        component(item, builder -> {
+            builder.set(type, t);
+        });
+    }
+
+    private static <T> void component(Item item, Consumer<DataComponentMap.Builder> consumer) {
         DataComponentMap.Builder builder = DataComponentMap.builder();
         builder.addAll(item.components);
-        builder.set(type, t);
+        consumer.accept(builder);
         item.components = builder.build();
     }
 }
--- src/main/java/org/polaris2023/wild_wind/common/item/food/NetherMushroomStewItem.java
+++ src/main/java/org/polaris2023/wild_wind/common/item/food/NetherMushroomStewItem.java
@@ -3,9 +3,13 @@ package org.polaris2023.wild_wind.common.item.food;
 import net.minecraft.core.component.DataComponents;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.monster.ZombifiedPiglin;
+import net.minecraft.world.entity.monster.hoglin.Hoglin;
+import net.minecraft.world.entity.monster.piglin.Piglin;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.AABB;
 import org.polaris2023.wild_wind.common.init.ModFoods;
 import org.polaris2023.wild_wind.common.item.BasicItem;
 
@@ -22,6 +26,12 @@ public class NetherMushroomStewItem extends BasicItem {
         if (ModFoods.NETHER_MUSHROOM_STEW.get().equals(stack.get(DataComponents.FOOD))) {
             entity.removeEffect(MobEffects.WITHER);
             entity.clearFire();
+            for (Piglin piglin : level.getEntitiesOfClass(Piglin.class, new AABB(entity.getOnPos()).inflate(7))) {
+                if (entity.equals(piglin.getLastHurtByMob())) {
+                    entity.setLastHurtByMob(null);
+                }
+
+            }
         }
         return super.finishUsingItem(stack, level, entity);
     }
