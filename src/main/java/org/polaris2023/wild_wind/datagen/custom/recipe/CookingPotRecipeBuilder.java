df42358d6f34720742d21bdf73407374727ed686
baka4n <qq474899581@gmail.com>
2024-11-25 17:17:39 +0800

    给配方添加流体接口

--- src/main/java/org/polaris2023/wild_wind/datagen/custom/recipe/CookingPotRecipeBuilder.java
+++ src/main/java/org/polaris2023/wild_wind/datagen/custom/recipe/CookingPotRecipeBuilder.java
@@ -14,6 +14,7 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.ItemLike;
+import net.neoforged.neoforge.fluids.FluidStack;
 import org.jetbrains.annotations.Nullable;
 import org.polaris2023.wild_wind.common.recipe.CookingPotRecipe;
 
@@ -29,30 +30,32 @@ public class CookingPotRecipeBuilder implements RecipeBuilder {
     private final ItemStack resultStack; // Neo: add stack result support
     private final NonNullList<Ingredient> ingredients = NonNullList.create();
     private final Map<String, Criterion<?>> criteria = new LinkedHashMap<>();
+    private final FluidStack stack;
     @Nullable
     private String group;
 
-    public CookingPotRecipeBuilder(RecipeCategory category, ItemLike result, int count) {
-        this(category, new ItemStack(result, count));
+    public CookingPotRecipeBuilder(RecipeCategory category, ItemLike result, int count, FluidStack stack) {
+        this(category, new ItemStack(result, count), stack);
     }
 
-    public CookingPotRecipeBuilder(RecipeCategory category, ItemStack itemStack) {
+    public CookingPotRecipeBuilder(RecipeCategory category, ItemStack result, FluidStack stack) {
         this.category = category;
-        this.result = itemStack.getItem();
-        this.count = itemStack.getCount();
-        this.resultStack = itemStack;
+        this.result = result.getItem();
+        this.count = result.getCount();
+        this.resultStack = result;
+        this.stack = stack;
     }
 
-    public static CookingPotRecipeBuilder cooking(RecipeCategory category, ItemLike result) {
-        return new CookingPotRecipeBuilder(category, result, 1);
+    public static CookingPotRecipeBuilder cooking(RecipeCategory category, ItemLike result, FluidStack stack) {
+        return new CookingPotRecipeBuilder(category, result, 1, stack);
     }
 
-    public static CookingPotRecipeBuilder cooking(RecipeCategory category, ItemLike result, int count) {
-        return new CookingPotRecipeBuilder(category, result, count);
+    public static CookingPotRecipeBuilder cooking(RecipeCategory category, ItemLike result, int count, FluidStack stack) {
+        return new CookingPotRecipeBuilder(category, result, count, stack);
     }
 
-    public static CookingPotRecipeBuilder cooking(RecipeCategory p_252339_, ItemStack result) {
-        return new CookingPotRecipeBuilder(p_252339_, result);
+    public static CookingPotRecipeBuilder cooking(RecipeCategory category, ItemStack result, FluidStack stack) {
+        return new CookingPotRecipeBuilder(category, result, stack);
     }
 
     @Override
@@ -82,12 +85,13 @@ public class CookingPotRecipeBuilder implements RecipeBuilder {
                 .rewards(AdvancementRewards.Builder.recipe(id))
                 .requirements(AdvancementRequirements.Strategy.OR);
         this.criteria.forEach(builder::addCriterion);
-        CookingPotRecipe shapelessrecipe = new CookingPotRecipe(
+        CookingPotRecipe cookingPot = new CookingPotRecipe(
                 Objects.requireNonNullElse(this.group, ""),
                 RecipeBuilder.determineBookCategory(this.category),
                 this.resultStack,
+                stack,
                 this.ingredients
         );
-        recipeOutput.accept(id, shapelessrecipe, builder.build(id.withPrefix("recipes/" + this.category.getFolderName() + "/")));
+        recipeOutput.accept(id, cookingPot, builder.build(id.withPrefix("recipes/" + this.category.getFolderName() + "/")));
     }
 }
