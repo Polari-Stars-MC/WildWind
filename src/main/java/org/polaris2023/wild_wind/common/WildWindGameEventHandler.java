77fb56f305921f0a3514702b1cf771ba0c025073
baka4n <qq474899581@gmail.com>
2024-12-08 19:47:25 +0800

    饱食度显示

--- src/main/java/org/polaris2023/wild_wind/common/WildWindGameEventHandler.java
+++ src/main/java/org/polaris2023/wild_wind/common/WildWindGameEventHandler.java
@@ -2,9 +2,11 @@ package org.polaris2023.wild_wind.common;
 
 import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
 import net.minecraft.core.component.DataComponentType;
+import net.minecraft.core.component.DataComponents;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.npc.VillagerTrades;
+import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.ItemStack;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
@@ -27,6 +29,13 @@ public class WildWindGameEventHandler {
     public static void tooltipAdd(ItemTooltipEvent event) {
         ItemStack stack = event.getItemStack();
         List<Component> toolTip = event.getToolTip();
+        if (stack.has(DataComponents.FOOD)) {
+            FoodProperties foodProperties = stack.get(DataComponents.FOOD);
+            assert foodProperties != null;
+            toolTip.addLast(Component.empty().append(ModTranslateKey.NUTRITION.translatable()).append(String.valueOf(foodProperties.nutrition())));
+            toolTip.addLast(Component.empty().append(ModTranslateKey.SATURATION.translatable()).append(String.valueOf(foodProperties.saturation())));
+
+        }
         componentAdd(stack, toolTip, ModTranslateKey.MEAT_VALUE, ModComponents.MEAT_VALUE, 0F);
         componentAdd(stack, toolTip, ModTranslateKey.VEGETABLE_VALUE, ModComponents.VEGETABLE_VALUE, 0F);
         componentAdd(stack, toolTip, ModTranslateKey.FRUIT_VALUE, ModComponents.FRUIT_VALUE, 0F);
