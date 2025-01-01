2784c0e8614b965a090d6fdf8025fbe2cf4113dd
刘冬煜 <10185101162@stu.ecnu.edu.cn>
2025-01-02 00:07:47 +0800

    feat[brittle_ice]: 世界生成

---
+++ src/main/java/org/polaris2023/wild_wind/datagen/worldgen/ModConfiguredFeatureRegistry.java
@@ -0,0 +1,21 @@
+package org.polaris2023.wild_wind.datagen.worldgen;
+
+import net.minecraft.data.worldgen.BootstrapContext;
+import net.minecraft.data.worldgen.features.FeatureUtils;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
+import net.minecraft.world.level.levelgen.feature.Feature;
+import net.minecraft.world.level.levelgen.feature.configurations.OreConfiguration;
+import net.minecraft.world.level.levelgen.structure.templatesystem.BlockMatchTest;
+import net.minecraft.world.level.levelgen.structure.templatesystem.RuleTest;
+import org.polaris2023.wild_wind.common.init.ModBlocks;
+
+public class ModConfiguredFeatureRegistry {
+	public static final ResourceKey<ConfiguredFeature<?, ?>> BRITTLE_ICE = FeatureUtils.createKey("brittle_ice");
+
+	public static void bootstrap(BootstrapContext<ConfiguredFeature<?, ?>> context) {
+		RuleTest isIce = new BlockMatchTest(Blocks.ICE);
+		FeatureUtils.register(context, BRITTLE_ICE, Feature.ORE, new OreConfiguration(isIce, ModBlocks.BRITTLE_ICE.get().defaultBlockState(), 10));
+	}
+}
---
+++ src/main/java/org/polaris2023/wild_wind/datagen/worldgen/ModPlacedFeatureRegistry.java
@@ -0,0 +1,28 @@
+package org.polaris2023.wild_wind.datagen.worldgen;
+
+import net.minecraft.core.HolderGetter;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.worldgen.BootstrapContext;
+import net.minecraft.data.worldgen.placement.PlacementUtils;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.level.levelgen.VerticalAnchor;
+import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
+import net.minecraft.world.level.levelgen.placement.*;
+
+import java.util.List;
+
+public class ModPlacedFeatureRegistry {
+	public static final ResourceKey<PlacedFeature> BRITTLE_ICE = PlacementUtils.createKey("brittle_ice");
+
+	public static void bootstrap(BootstrapContext<PlacedFeature> context) {
+		HolderGetter<ConfiguredFeature<?, ?>> configuredFeaturesLookup = context.lookup(Registries.CONFIGURED_FEATURE);
+		PlacementUtils.register(
+				context, BRITTLE_ICE, configuredFeaturesLookup.getOrThrow(ModConfiguredFeatureRegistry.BRITTLE_ICE),
+				orePlacement(CountPlacement.of(2), HeightRangePlacement.uniform(VerticalAnchor.absolute(60), VerticalAnchor.absolute(63)))
+		);
+	}
+
+	private static List<PlacementModifier> orePlacement(PlacementModifier countPlacement, PlacementModifier heightRange) {
+		return List.of(countPlacement, InSquarePlacement.spread(), heightRange, BiomeFilter.biome());
+	}
+}
