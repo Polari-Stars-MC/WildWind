a4d2010d6cf95a37dcbadc011b9c122d16112512
baka4n <qq474899581@gmail.com>
2025-02-06 19:12:23 +0800

    Update WildWindClientEventHandler.java

--- src/main/java/org/polaris2023/wild_wind/client/WildWindClientEventHandler.java
+++ src/main/java/org/polaris2023/wild_wind/client/WildWindClientEventHandler.java
@@ -6,6 +6,7 @@ import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.EventPriority;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.client.event.EntityRenderersEvent;
 import net.neoforged.neoforge.client.event.RegisterColorHandlersEvent;
 import org.polaris2023.wild_wind.common.init.ModComponents;
 
@@ -15,5 +16,10 @@ public class WildWindClientEventHandler {
     public static void registerSlimeColor(RegisterColorHandlersEvent.Item event) {
         event.register((stack, tintIndex) ->
                 FastColor.ARGB32.opaque(stack.getOrDefault(ModComponents.SLIME_COLOR, 0)), Items.SLIME_BALL);
+    }
+
+    @SubscribeEvent
+    public static void registerRender(EntityRenderersEvent.RegisterRenderers event) {
+
     }
 }
