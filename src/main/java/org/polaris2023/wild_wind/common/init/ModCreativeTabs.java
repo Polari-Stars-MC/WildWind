cb80c92e6ebfee9be10d7b30b4a858ef406fd394
baka4n <qq474899581@gmail.com>
2025-02-12 19:19:19 +0800

    添加材质纹理，对物品进行分类

--- src/main/java/org/polaris2023/wild_wind/common/init/ModCreativeTabs.java
+++ src/main/java/org/polaris2023/wild_wind/common/init/ModCreativeTabs.java
@@ -20,6 +20,22 @@ import static org.polaris2023.wild_wind.common.init.ModInitializer.TABS;
 
 @EventBusSubscriber(bus = EventBusSubscriber.Bus.MOD, modid = WildWindMod.MOD_ID)
 public enum ModCreativeTabs implements Supplier<CreativeModeTab> {
+    @I18n(en_us = "Wild wind: Building block", zh_cn = "原野之风：建筑方块", zh_tw = "原野之風：建築方塊")
+    BUILDING_BLOCK(ModBlocks.BRITTLE_ICE::toStack, () -> (__, output) -> {
+        output.accept(ModBlocks.BRITTLE_ICE_ITEM);
+    }),
+    @I18n(en_us = "Wild wind: Natural block", zh_cn = "原野之风：自然方块", zh_tw = "原野之風：自然方塊")
+    NATURAL_BLOCKS(ModBlocks.SALT_BLOCK_ITEM::toStack, () -> (__, output) -> {
+        output.accept(ModBlocks.SALT_BLOCK_ITEM);
+    }),
+    @I18n(en_us = "Wild wind: Tools and Utilities", zh_cn = "原野之风：工具与实用物品", zh_tw = "原野之風：工具與實用物品")
+    TOOLS_AND_UTILITIES(ModItems.MAGIC_FLUTE::toStack, () -> (__, output) -> {
+        output.accept(ModItems.MAGIC_FLUTE);
+        output.accept(Items.BUCKET);
+        output.accept(ModItems.PIRANHA_BUCKET);
+        output.accept(ModItems.TROUT_BUCKET);
+
+    }),
     @I18n(en_us = "Wild wind: Food & drink", zh_cn = "原野之风：食物与饮品", zh_tw = "原野之風：食物與飲品")
     FOOD_AND_DRINK(ModItems.PUMPKIN_SLICE::toStack,
             () -> (__, output) -> {
@@ -32,11 +48,32 @@ public enum ModCreativeTabs implements Supplier<CreativeModeTab> {
                     }
                 }
             }),
-    @I18n(en_us = "Wild Wind Tags", zh_cn = "原野之风", zh_tw = "原野之風")
-    WILD_WIND(ModBlocks.GLOW_MUCUS_ITEM::toStack,
+    @I18n(en_us = "Wild wind: Ingredients", zh_cn = "原野之风：原材料", zh_tw = "原野之風：原材料")
+    INGREDIENTS(ModBlocks.GLOW_MUCUS_ITEM::toStack, () -> (__, output) -> {
+        output.accept(ModBlocks.GLOW_MUCUS_ITEM);
+        output.accept(ModBlocks.GLAREFLOWER_ITEM);
+        output.accept(ModBlocks.GLAREFLOWER_SEEDS_ITEM);
+        output.accept(ModBlocks.REEDS_ITEM);
+        output.accept(ModBlocks.CATTAILS_ITEM);
+    }),
+    @I18n(en_us = "Wild wind: Spawn Eggs", zh_cn = "原野之风：刷怪蛋", zh_tw = "原野之風：生怪蛋")
+    SPAWN_EGGS(ModItems.FIREFLY_SPAWN_EGG::toStack, () -> (__, output) -> {
+        output.accept(ModItems.FIREFLY_SPAWN_EGG);
+        output.accept(ModItems.GLARE_SPAWN_EGG);
+        output.accept(ModItems.PIRANHA_SPAWN_EGG);
+        output.accept(ModItems.TROUT_SPAWN_EGG);
+    }),
+    @I18n(en_us = "Wild wind: Misc", zh_cn = "原野之风：杂项", zh_tw = "原野之風：雜項")
+    WILD_WIND(ModBlocks.COOKING_POT_ITEM::toStack,
             () -> (__, output) -> {
                 for (DeferredHolder<Item, ? extends Item> item : ModInitializer.items()) {
-                    if (check(item, FOOD_AND_DRINK)) {
+                    if (checkOr(item,
+                            FOOD_AND_DRINK,
+                            BUILDING_BLOCK,
+                            TOOLS_AND_UTILITIES,
+                            NATURAL_BLOCKS,
+                            INGREDIENTS
+                    )) {
                         output.accept(item.get());
                     }
                 }
@@ -47,6 +84,16 @@ public enum ModCreativeTabs implements Supplier<CreativeModeTab> {
             }),
 
     ;
+
+    @SafeVarargs
+    private static <T extends Item> boolean checkOr(DeferredHolder<Item, T> item, Supplier<CreativeModeTab>... suppliers) {
+        for (Supplier<CreativeModeTab> supplier : suppliers) {
+            if (!check(item, supplier)) {
+                return false;
+            }
+        }
+        return true;
+    }
 
     private static <T extends Item> boolean check(DeferredHolder<Item, T> item, Supplier<CreativeModeTab> supplier) {
         return supplier.get().getDisplayItems().stream().filter(stack -> stack.is(item)).findFirst().isEmpty();
