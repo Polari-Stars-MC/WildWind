2558f0ef7557f06ee3c35d3967692862a41691f3
ENC_Euphony <2330458484@qq.com>
2025-03-15 02:11:04 +0800

    修复灰烬模型，加入隐形展示框生成时的声音和粒子效果

--- src/main/java/org/polaris2023/wild_wind/common/event/InvisibleItemFrameEvent.java
+++ src/main/java/org/polaris2023/wild_wind/common/event/InvisibleItemFrameEvent.java
@@ -1,11 +1,20 @@
 package org.polaris2023.wild_wind.common.event;
 
+import net.minecraft.client.particle.AshParticle;
+import net.minecraft.client.particle.FallingDustParticle;
+import net.minecraft.core.particles.BlockParticleOption;
+import net.minecraft.core.particles.ParticleOptions;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.decoration.ItemFrame;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.phys.HitResult;
 import org.polaris2023.wild_wind.common.init.ModItems;
 import org.polaris2023.wild_wind.common.inter.ICustomItemFrame;
@@ -24,6 +33,13 @@ public class InvisibleItemFrameEvent {
                     return InteractionResult.PASS;
 
                 frame.wild_wind$setIsInvisible(true);
+
+                ItemFrame itemFrame = (ItemFrame) entity;
+                itemFrame.playSound(SoundEvents.BREEZE_LAND, 1.0f, 1.0f);
+
+                ((ServerLevel) world).sendParticles(new BlockParticleOption(ParticleTypes.FALLING_DUST, Blocks.GRAVEL.defaultBlockState()),
+                        itemFrame.getX(), itemFrame.getY(), itemFrame.getZ(),
+                        10, 0.5, 0.5, 0.5, 0.1);
 
                 player.getItemInHand(hand).consume(1, player);
                 return InteractionResult.SUCCESS;
