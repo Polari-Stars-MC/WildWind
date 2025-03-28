abc3cfb76f57a4c8725599a7bb9d4fe2e772b38c
glyceryl <494192175@qq.com>
2025-03-28 23:53:39 +0800

    删除一些冗余的代码。

--- processor/src/main/java/org/polaris2023/processor/clazz/datagen/ModelProcessor.java
+++ processor/src/main/java/org/polaris2023/processor/clazz/datagen/ModelProcessor.java
@@ -197,21 +197,21 @@ public class ModelProcessor extends ClassProcessor {
         }
     }
 
-    private void cubeAllFor(TypeElement typeElement, VariableElement variableElement) {
-        CubeAllFor cubeAllFor = variableElement.getAnnotation(CubeAllFor.class);
-        if (cubeAllFor.self()) {
-            CubeAll cube = cubeAllFor.cube();
-            String sb = "cubeAll(%s.%s, %s, \"%s\", \"%s\");"
-                    .formatted(
-                            typeElement.getQualifiedName(),
-                            variableElement.getSimpleName(),
-                            cube != null && cube.item(),
-                            cube != null ? cube.render_type() : "",
-                            cube != null ? cube.all() : ""
-                    );
-            InitProcessor.modelGen(context, sb);
-        }
-    }
+//    private void cubeAllFor(TypeElement typeElement, VariableElement variableElement) {
+//        CubeAllFor cubeAllFor = variableElement.getAnnotation(CubeAllFor.class);
+//        if (cubeAllFor.self()) {
+//            CubeAll cube = cubeAllFor.cube();
+//            String sb = "cubeAll(%s.%s, %s, \"%s\", \"%s\");"
+//                    .formatted(
+//                            typeElement.getQualifiedName(),
+//                            variableElement.getSimpleName(),
+//                            cube != null && cube.item(),
+//                            cube != null ? cube.render_type() : "",
+//                            cube != null ? cube.all() : ""
+//                    );
+//            InitProcessor.modelGen(context, sb);
+//        }
+//    }
 
     private void basicBlock(TypeElement typeElement, VariableElement variableElement) {
         BasicBlock basicBlock = variableElement.getAnnotation(BasicBlock.class);
