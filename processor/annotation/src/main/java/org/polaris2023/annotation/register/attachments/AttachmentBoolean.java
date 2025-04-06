3262dfad83b690c1a332802d76ba914643411c3a
baka4n <qq474899581@gmail.com>
2025-04-06 19:53:38 +0800

    注解擦除

--- processor/annotation/src/main/java/org/polaris2023/annotation/register/attachments/AttachmentBoolean.java
+++ processor/annotation/src/main/java/org/polaris2023/annotation/register/attachments/AttachmentBoolean.java
@@ -1,9 +1,13 @@
 package org.polaris2023.annotation.register.attachments;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 /**
  * @author : baka4n
  * {@code @Date : 2025/04/06 19:45:25}
  */
+@Retention(RetentionPolicy.SOURCE)
 public @interface AttachmentBoolean {
     boolean defaultValue() default false;
 }
