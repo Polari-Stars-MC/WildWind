c2f74730481f5a5d3e461f1ed27073b9f497b6df
刘冬煜 <10185101162@stu.ecnu.edu.cn>
2025-04-10 23:39:55 +0800

    feat[cattails & reeds]: 允许方块含水

--- src/main/java/org/polaris2023/wild_wind/common/block/ReedsBlock.java
+++ src/main/java/org/polaris2023/wild_wind/common/block/ReedsBlock.java
@@ -1,15 +1,67 @@
 package org.polaris2023.wild_wind.common.block;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.DoublePlantBlock;
+import net.minecraft.world.level.block.SimpleWaterloggedBlock;
 import net.minecraft.world.level.block.TallFlowerBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.level.block.state.properties.DoubleBlockHalf;
+import net.minecraft.world.level.material.FluidState;
+import net.minecraft.world.level.material.Fluids;
+import net.minecraft.world.phys.Vec3;
 import net.neoforged.neoforge.common.IShearable;
 import org.polaris2023.wild_wind.common.init.tags.ModBlockTags;
 
-public class ReedsBlock extends TallFlowerBlock implements IShearable {
+import javax.annotation.Nullable;
+
+public class ReedsBlock extends TallFlowerBlock implements IShearable, SimpleWaterloggedBlock {
+    public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
+
     public ReedsBlock(Properties props) {
         super(props);
+        this.registerDefaultState(this.stateDefinition.any().setValue(HALF, DoubleBlockHalf.LOWER).setValue(WATERLOGGED, false));
+    }
+
+    @Override
+    protected void entityInside(BlockState state, Level level, BlockPos pos, Entity entity) {
+        Vec3 vec3 = new Vec3(0.5F, 0.25F, 0.5F);
+
+        entity.makeStuckInBlock(state, vec3);
+    }
+
+    @Override
+    protected FluidState getFluidState(BlockState state) {
+        return state.getValue(WATERLOGGED) ? Fluids.WATER.getSource(false) : super.getFluidState(state);
+    }
+
+    @Override @Nullable
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        BlockState blockstate = super.getStateForPlacement(context);
+        return blockstate != null ? copyWaterloggedFrom(context.getLevel(), context.getClickedPos(), blockstate) : null;
+    }
+
+    @Override
+    public void setPlacedBy(Level level, BlockPos pos, BlockState state, LivingEntity placer, ItemStack itemStack) {
+        if (!level.isClientSide()) {
+            BlockPos blockpos = pos.above();
+            BlockState blockstate = DoublePlantBlock.copyWaterloggedFrom(level, blockpos, this.defaultBlockState().setValue(HALF, DoubleBlockHalf.UPPER));
+            level.setBlock(blockpos, blockstate, Block.UPDATE_ALL);
+        }
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(HALF, WATERLOGGED);
     }
 
     @Override
