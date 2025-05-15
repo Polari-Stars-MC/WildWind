b168a6a2ba310d67bfa3c179a4d4d96888d962f4
baka4n <qq474899581@gmail.com>
2025-05-15 23:55:51 +0800

    neo toml生成

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,24 @@
+import org.polaris2023.mcmeta.extension.McMetaSettings
+import org.polaris2023.mcmeta.extension.forge.ForgeLikeDependency
+import org.polaris2023.mcmeta.extension.forge.ForgeLikeDependency.Order
+import org.polaris2023.mcmeta.extension.forge.ForgeLikeDependency.Side
+import org.polaris2023.mcmeta.extension.forge.ForgeLikeToml
+import org.polaris2023.mcmeta.extension.forge.neo.NeoForgeDependency
+import org.polaris2023.mcmeta.extension.forge.neo.NeoForgeDependency.Type
+import org.polaris2023.mcmeta.extension.forge.neo.NeoForgeMods
+import org.polaris2023.mcmeta.extension.forge.neo.NeoForgeModsToml
 import org.slf4j.event.Level
-import java.util.Properties
+
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+
+    dependencies {
+        classpath("io.github.polari-stars-mc:mcmeta-plugin:0.0.2-fix-5")
+    }
+
+}
 
 plugins {
     `java-library`
@@ -12,7 +31,7 @@ plugins {
 val mcVersion: String by rootProject
 val mcVersionRange: String by rootProject
 val modGroupId: String by rootProject
-val modAuthors: String by rootProject
+
 val modLicense: String by rootProject
 val neoVersion: String by rootProject
 val neoVersionRange: String by rootProject
@@ -23,6 +42,14 @@ allprojects {
     apply(plugin = "maven-publish")
     apply(plugin = "java-library")
     apply(plugin = "idea")
+    apply(plugin = "io.github.Polari-Stars-MC.mcmeta-plugin")
+
+    configure<ForgeLikeToml> {
+        loaderVersion = loaderVersionRange
+        license = modLicense
+        issueTrackerURL= uri("https://github.com/Polari-Stars-MC/WildWind/issues")
+    }
+
     repositories {
         mavenLocal()
         mavenCentral()
@@ -45,29 +72,16 @@ val lib = libs
 
 subprojects {
     apply(plugin = lib.plugins.mod.dev.gradle.get().pluginId)
-
-    val props = Properties()
-    val propsRoot = Properties()
-    val pp = file("gradle.properties")
-    val ppRoot = rootProject.file("gradle.properties")
-    if (pp.exists()) {
-        pp.bufferedReader(Charsets.UTF_8).use {
-            props.load(it)
-        }
+    configure<McMetaSettings> {
+        loaderType = McMetaSettings.Type.NEOFORGE
     }
-    if (ppRoot.exists()) {
-        ppRoot.bufferedReader(Charsets.UTF_8).use {
-            propsRoot.load(it)
-        }
-    }
-
-
-
 
     val modId: String by project
     val modName: String by project
     val modVersion: String by project
     val modClassPrefix: String by project
+
+
 
 
     base {
@@ -76,6 +90,43 @@ subprojects {
     description = modName
     version = "$mcVersion-$modVersion"
     group = modGroupId
+    val modAuthors: String by project
+
+    configure<NeoForgeModsToml> {
+        mods.add(NeoForgeMods(project).apply {
+            this.modId = modId
+            version = modVersion
+            displayName = modName
+            authors = modAuthors
+            logoFile = "$modId.png"
+            displayURL = uri("https://github.com/Polari-Stars-MC/WildWind")
+            description = "This is $modName."
+        })
+        dependencies().put(modId, arrayOf(
+            NeoForgeDependency
+                .builder()
+                .type(Type.required)
+                .like(ForgeLikeDependency
+                    .builder()
+                    .modId("neoforge")
+                    .versionRange(neoVersionRange)
+                    .ordering(Order.NONE)
+                    .side(Side.BOTH)
+                    .build())
+                .build(),
+            NeoForgeDependency
+                .builder()
+                .type(Type.required)
+                .like(ForgeLikeDependency
+                    .builder()
+                    .modId("minecraft")
+                    .versionRange(mcVersionRange)
+                    .ordering(Order.NONE)
+                    .side(Side.BOTH)
+                    .build())
+                .build(),
+        ))
+    }
 
     val replaceProperties: Map<String, String> = mapOf(
         "minecraft_version" to mcVersion,
